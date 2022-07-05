b88ccb2e9ba7ff4376223c2a8ca6b0956bac6d72
Bui Van Thai <buithaild230899@gmail.com>
2022-07-05 14:38:44 +0700

    mvc

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,12 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('Hello world from Bui Thai')
-    });
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+
     return app.use("/", router);
 
 }
