c1f39174ca985766a9749ac77d06a717f032aae4
Bui Van Thai <buithaild230899@gmail.com>
2022-07-06 11:13:09 +0700

    crud 1

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
-import db from '../models/index'
+import db from '../models/index';
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -15,7 +16,21 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('Post crud from server');
+}
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
